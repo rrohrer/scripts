fb3c3da906e5a5baa509b040a5052ef48e8a0fc8
rrohrer <ryan.rohrer@gmail.com>
2023-02-11 10:38:32 -0800

    Changed colors to material gruvbox.

--- config_backup/.config/nvim/after/plugin/colors.lua
+++ config_backup/.config/nvim/after/plugin/colors.lua
@@ -1,11 +1,13 @@
 function EnsureCustomColorTheme(color)
-  color = color or "gruvbox"
+  color = color or "gruvbox-material"
+  vim.g.gruvbox_material_background = "soft"
+  vim.g.gruvbox_material_foreground = "material"
   vim.cmd.colorscheme(color)
 
   -- This fixes colors for the floating windows and respects transparency controls.
   -- Basically its telling nvim to ignore bg color.
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 EnsureCustomColorTheme()
--- config_backup/.config/nvim/lua/rrohrer/packer.lua
+++ config_backup/.config/nvim/lua/rrohrer/packer.lua
@@ -2,53 +2,49 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+      -- Packer can manage itself
+      use 'wbthomason/packer.nvim'
 
-  -- Telescope is the fuzzy file finder!
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+      -- Telescope is the fuzzy file finder!
+      use {
+          'nvim-telescope/telescope.nvim', tag = '0.1.1',
+          requires = { { 'nvim-lua/plenary.nvim' } }
+      }
 
-  -- Color theme: GruvBox
-  use {
-	  'gruvbox-community/gruvbox',
-	  config = function()
-		  vim.cmd('colorscheme gruvbox')
-	  end
-  }
+      -- Color theme: GruvBox and gruvbox material
+      use('gruvbox-community/gruvbox')
+      use('sainnhe/gruvbox-material')
 
-  -- TreeSitter for enhanced highlighting
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+      -- TreeSitter for enhanced highlighting
+      use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
 
-  -- Harpoon allows jumping around files.
-  use('theprimeagen/harpoon')
+      -- Harpoon allows jumping around files.
+      use('theprimeagen/harpoon')
 
-  -- Undotree gives you a full tree for undo/redo.
-  use('mbbill/undotree')
+      -- Undotree gives you a full tree for undo/redo.
+      use('mbbill/undotree')
 
-  --LSP Setup
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      --LSP Setup
+      use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v1.x',
+          requires = {
+              -- LSP Support
+              { 'neovim/nvim-lspconfig' }, -- Required
+              { 'williamboman/mason.nvim' }, -- Optional
+              { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+              -- Autocompletion
+              { 'hrsh7th/nvim-cmp' }, -- Required
+              { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+              { 'hrsh7th/cmp-buffer' }, -- Optional
+              { 'hrsh7th/cmp-path' }, -- Optional
+              { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+              { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
-    }
-  }
-end)
+              -- Snippets
+              { 'L3MON4D3/LuaSnip' }, -- Required
+              { 'rafamadriz/friendly-snippets' }, -- Optional
+          }
+      }
+    end)
