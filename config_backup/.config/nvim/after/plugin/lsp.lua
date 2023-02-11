d2dfb1620c4089fa39c552c4f1126d7aa2eb8771
rrohrer <ryan.rohrer@gmail.com>
2023-02-10 18:35:03 -0800

    Add format on save and prettier formatting.

--- config_backup/.config/nvim/after/plugin/lsp.lua
+++ config_backup/.config/nvim/after/plugin/lsp.lua
@@ -6,33 +6,33 @@ lsp.preset('recommended')
 lsp.nvim_workspace()
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'sumneko_lua',
-  'rust_analyzer',
-  'clangd',
-  'bashls',
+    'tsserver',
+    'eslint',
+    'sumneko_lua',
+    'rust_analyzer',
+    'clangd',
+    'bashls',
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({select = true}),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
 
 lsp.set_preferences({
-  sign_icons = {}
+    sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -47,3 +47,9 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
+
+-- Format on save.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    command = "lua vim.lsp.buf.formatting_sync()",
+})
