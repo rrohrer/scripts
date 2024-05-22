b86be52db70429a4d0de3dae8fd813ee0f9009a7
rrohrer <ryan.rohrer@gmail.com>
2024-05-22 01:04:22 -0700

    Migrated alacritty config

--- backup_config.sh
+++ backup_config.sh
@@ -4,7 +4,7 @@ mkdir -p ~/scripts/config_backup
 # backup tmux config
 cp ~/.tmux.conf ~/scripts/config_backup/.tmux.conf
 
-# backup neovim config 
+# backup neovim config
 # mkdir -p ~/scripts/config_backup/.config/nvim/lua/user
 # cp ~/.config/nvim/lua/user/init.lua ~/scripts/config_backup/.config/nvim/lua/user/init.lua
 mkdir -p ~/scripts/config_backup/.config/nvim/
@@ -14,6 +14,7 @@ rm -rf ~/scripts/config_backup/.config/nvim/plugin/
 # backup alacritty config
 mkdir -p ~/scripts/config_backup/.config/alacritty
 cp ~/.config/alacritty/alacritty.yml ~/scripts/config_backup/.config/alacritty/alacritty.yml
+cp ~/.config/alacritty/alacritty.toml ~/scripts/config_backup/.config/alacritty/alacritty.toml
 
 # backup helix config
 mkdir -p ~/scripts/config_backup/.config/helix
--- restore_config.sh
+++ restore_config.sh
@@ -7,10 +7,11 @@ mkdir -p ~/.config/helix/themes/
 mkdir -p ~/.config/zellij/
 
 # copy the files
-cp ~/scripts/config_backup/.tmux.conf ~/.tmux.conf 
+cp ~/scripts/config_backup/.tmux.conf ~/.tmux.conf
 # cp ~/scripts/config_backup/.config/nvim/lua/user/init.lua ~/.config/nvim/lua/user/init.lua
 cp -r ~/scripts/config_backup/.config/nvim/ ~/.config/nvim/
 cp ~/scripts/config_backup/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ~/scripts/config_backup/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/scripts/config_backup/.config/helix/config.toml ~/.config/helix/config.toml
 cp -r ~/scripts/config_backup/.config/helix/themes/ ~/.config/helix/themes/
 cp ~/scripts/config_backup/.config/zellij/config.kdl ~/.config/zellij/config.kdl
