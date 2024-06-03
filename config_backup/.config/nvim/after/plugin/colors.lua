ce5f415706031178a244ee5a897c6421e4a241ae
rrohrer <ryan.rohrer@gmail.com>
2024-06-03 15:34:01 -0700

    fixing tmux and nvim a bit

--- config_backup/.config/nvim/after/plugin/colors.lua
+++ config_backup/.config/nvim/after/plugin/colors.lua
@@ -1,13 +1,20 @@
 function EnsureCustomColorTheme(color)
   color = color or "gruvbox-material"
+  -- Configure gruvbox material
   vim.g.gruvbox_material_background = "soft"
   vim.g.gruvbox_material_foreground = "material"
+  vim.g.gruvbox_material_diagnostic_text_highlight = 1
+  vim.g.gruvbox_material_diagnostic_line_highlight = 1
+  vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+
+  -- Set the color scheme.
+  vim.opt.termguicolors = true
   vim.cmd.colorscheme(color)
 
   -- This fixes colors for the floating windows and respects transparency controls.
   -- Basically its telling nvim to ignore bg color.
-  --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 EnsureCustomColorTheme()
