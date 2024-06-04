f873af944f2f7202d31dfdcae07e2bbd67c491e1
rrohrer <ryan.rohrer@gmail.com>
2024-06-03 17:44:53 -0700

    Fixed Telescope Colors

--- config_backup/.config/nvim/after/plugin/colors.lua
+++ config_backup/.config/nvim/after/plugin/colors.lua
@@ -13,6 +13,11 @@ function EnsureCustomColorTheme(color)
   -- Set the color scheme.
   vim.cmd.colorscheme(color)
 
+  -- Customization for TelescopeBorder
+  vim.cmd [[highlight TelescopeBorder guifg=#7daea3]]
+  vim.cmd [[highlight DiffAdd guifg=#a9b665 guibg=#424a3e]]
+  vim.cmd [[highlight TelescopeSelection guifg=#7daea3 guibg=#404946]]
+
   -- This fixes colors for the floating windows and respects transparency controls.
   -- Basically its telling nvim to ignore bg color.
   -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
