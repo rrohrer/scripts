2764c42c1e86dfc356a19f5fec834f23b684bf91
rrohrer <ryan.rohrer@gmail.com>
2025-07-22 00:16:47 -0700

    fix todo highlighting

--- config_backup/.config/nvim/lua/rrohrer/lazy/colors.lua
+++ config_backup/.config/nvim/lua/rrohrer/lazy/colors.lua
@@ -22,6 +22,7 @@ function EnsureCustomColorTheme(color)
   -- Basically its telling nvim to ignore bg color.
   -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, '@text.note', { link = 'Todo' })
 end
 
 return {
